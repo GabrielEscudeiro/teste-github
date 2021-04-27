a4c0f45ac3cc622bd11ac5915791a622b18f3082
Gabriel Escudeiro <gabrielescudeiro1@gmail.com>
2021-04-27 01:03:15 -0300

    Parágrafos no TabTeste Adicionados

---
+++ src/components/TabTeste.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>Esse aqui faz parte do terceiro commit</p>
+    <p>Também faz parte do terceiro commit</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
